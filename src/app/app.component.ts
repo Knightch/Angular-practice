8b2cc6e866152ed7d68eff908d74787f2cbd08bd
MATHEMATICS TERRAINS <amankumarpandey2397@gmail.com>
2022-06-13 11:51:27 +0530

    diff directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simpleCRM';
+  successMsg = false;
+  successFlag = false;
+
+  friendList = [
+    {
+      firstName: "Jitu",
+      lastName: "gupta",
+      firstMeet: 3,
+      address: "Dhanuspuja school"
+    },
+    {
+      firstName: "Amarjit",
+      lastName: "Sharma",
+      firstMeet: 4,
+      address: "Mumbai"
+    },
+    {
+      firstName: "Sujit",
+      lastName: "Karmakar",
+      firstMeet: 5,
+      address: "Talwadanga"
+    },
+    {
+      firstName: "Saurav",
+      lastName: "Banarjee",
+      firstMeet: 11,
+      address: "Khadanpara"
+    },
+    {
+      firstName: "Sonam",
+      lastName: "Bhagat",
+      firstMeet: 11,
+      address: "Shitla Mandir"
+    },
+    {
+      firstName: "Alka",
+      lastName: "jaiswal",
+      firstMeet: 11,
+      address: "Khadanpara"
+    }
+  ]
+
+  superPower = ""
+  bgColor="#000"
+  textColor="#fff"
+  color = "blue"
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProfileModule } from './profile/profile.module';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ProfileModule
   ],
   providers: [],
   bootstrap: [AppComponent]
